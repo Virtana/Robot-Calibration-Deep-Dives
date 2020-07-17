1b5612cd634f54b7a7cbb59a43532e14136244ec
RebeccaGibbon <gibbonrm@gmail.com>
2020-07-17 16:28:18 -0400

    Changes made to CMakeLists.txt, my_2d_robo_state_publisher and launch file to fix cmake and roslaunch errors

--- my_2d_robot/src/my_2d_robo_state_publisher.cpp
+++ my_2d_robot/src/my_2d_robo_state_publisher.cpp
@@ -5,24 +5,28 @@
 
 int main(int argc, char** argv){
     ros::init(argc, argv, "my_2d_robo_state_publisher");
-    ros::NOdehandle n;
+    ros::NodeHandle n;
     ros::Publisher joint_pub = n.advertise<sensor_msgs::JointState>("joint_states", 1);
     ros::Rate loop_rate(30);
 
 
     sensor_msgs::JointState joint_state;
 
+    
     while(ros::ok){
         joint_state.header.stamp = ros::Time::now();
-        joint_state.name = joint1;
-        joint_state.position = {0,0}
-        joint_state.name = joint2;
-        joint_state.position = {0,0}
+        //one message per update, This is 2 since thre are 2 joints which are updated 
+        //at the same time
+        joint_state.name = {"joint1","joint2"};
+        //position-angle of rotation
+        joint_state.position = {0,3.14};
+ 
+        //send joint state
+        joint_pub.publish(joint_state);
+
+        loop_rate.sleep();
+     
     }
-
-    joint_pub.publish(joint_state);
-
-    loop_rate.sleep();
 
     return 0;
 }
