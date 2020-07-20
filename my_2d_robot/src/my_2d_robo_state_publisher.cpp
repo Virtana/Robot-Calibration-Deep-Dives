5f57b877726af466f5e0e976866f2734f2e52513
RebeccaGibbon <gibbonrm@gmail.com>
2020-07-20 13:24:46 -0400

    Changes made to state publisher and urdf

--- my_2d_robot/src/my_2d_robo_state_publisher.cpp
+++ my_2d_robot/src/my_2d_robo_state_publisher.cpp
@@ -1,17 +1,20 @@
 #include <string>
 #include <ros/ros.h>
 #include <sensor_msgs/JointState.h>
+#include "std_msgs/String.h"
 
 
 int main(int argc, char** argv){
     ros::init(argc, argv, "my_2d_robo_state_publisher");
     ros::NodeHandle n;
-    ros::Publisher joint_pub = n.advertise<sensor_msgs::JointState>("joint_states", 1);
+    ros::Publisher joint_pub = n.advertise<sensor_msgs::JointState>("joint_states", 1000);
     ros::Rate loop_rate(30);
 
 
     sensor_msgs::JointState joint_state;
-
+    
+    int joint1_pos = (rand()%4);
+    int joint2_pos = (rand()%4);
     
     while(ros::ok){
         joint_state.header.stamp = ros::Time::now();
@@ -19,7 +22,11 @@ int main(int argc, char** argv){
         //at the same time
         joint_state.name = {"joint1","joint2"};
         //position-angle of rotation
-        joint_state.position = {0,3.14};
+        joint_state.position = {joint1_pos,joint2_pos};
+
+        //idk what this does..........
+        std_msgs::String msg;
+        ROS_INFO("%s", msg.data.c_str());
  
         //send joint state
         joint_pub.publish(joint_state);
@@ -30,6 +37,7 @@ int main(int argc, char** argv){
 
     return 0;
 }
+
 
 
 
