810527fdaec4f3d038321a6ae7a6a10acc87043f
RebeccaGibbon <gibbonrm@gmail.com>
2020-07-22 12:38:57 -0400

    Changes made to CMakeLists, xml and launch files to accomodate subscriber node. Minor changes made to subscriber and publisher files.

--- my_2d_robot/src/joint_states_subscriber.cpp
+++ my_2d_robot/src/joint_states_subscriber.cpp
@@ -1,19 +1,46 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include <sensor_msgs/JointState.h>
 
-void joint_statesCallback(const std_msgs::String::ConstPtr& msg)
+void joint_statesCallback(const sensor_msgs::JointState::ConstPtr& msg)
 {
-  ROS_INFO("I heard: [%s]", msg->data.c_str());
+  ROS_INFO("I heard: [%F]", msg->position[0]);
 }
 
-int main (int argc, char **argv){
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "joint_states_subscriber");
+  ros::NodeHandle n;
+  ros::NodeHandle nh;
+  ros::Subscriber sub = n.subscribe("joint_states", 1000, joint_statesCallback);
 
-    ros::init(argc, argv, "joint_states_subscriber");
-    ros::NodeHandle n;
-    ros::Subscriber sub = n.subscribe("joint_states", 1000, joint_statesCallback);
+  ros::spin();
 
-    ros::spin();
+  // Calculating the end effector position.
+  // Hardcoding the lengths of each link.
+  float link_1 = 5.0;
+  float link_2 = 5.0;
 
+  // Joint angle received from state publisher.
+  // Gotta fix this.................
+  float Theta1_Measured = msg->position[0];
+  float Theta2_Measured = msg->position[1];
 
-    return 0;
+  // Joint angle offsets.
+  float Theta1_offset, Theta2_offset;
+
+  // Getting the joint angle offsets that are set in the launch file.
+  nh.getParam("Theta1_offset", Theta1_offset);
+  nh.getParam("Theta2_offset", Theta2_offset);
+
+  // Including offsets to joint angles.
+  float Theta1 = Theta1_Measured - Theta1_offset;
+  float Theta2 = Theta2_Measured - Theta2_offset;
+
+  // Calculating the end effector position.
+  float x, y;
+  x = (link_1 * cos(Theta1 * M_PI / 180)) + (link_2 * cos((Theta1 + Theta2) * M_PI / 180));
+  y = (link_1 * sin(Theta1 * M_PI / 180)) + (link_2 * sin((Theta1 + Theta2) * M_PI / 180));
+
+  return 0;
 }
--- my_2d_robot/src/my_2d_robo_state_publisher.cpp
+++ my_2d_robot/src/my_2d_robo_state_publisher.cpp
@@ -1,47 +1,45 @@
 #include <string>
+#include <cstdlib>
+#include <ctime>
 #include <ros/ros.h>
 #include <sensor_msgs/JointState.h>
 #include "std_msgs/String.h"
 
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "my_2d_robo_state_publisher");
+  ros::NodeHandle n;
+  ros::Publisher joint_pub = n.advertise<sensor_msgs::JointState>("joint_states", 1000);
+  ros::Rate loop_rate(30);
 
-int main(int argc, char** argv){
-    ros::init(argc, argv, "my_2d_robo_state_publisher");
-    ros::NodeHandle n;
-    ros::Publisher joint_pub = n.advertise<sensor_msgs::JointState>("joint_states", 1000);
-    ros::Rate loop_rate(30);
+  sensor_msgs::JointState joint_state;
 
+  srand(time(0));
 
-    sensor_msgs::JointState joint_state;
-    
+  while (ros::ok)
+  {
+    joint_state.header.stamp = ros::Time::now();
 
-    
-    while(ros::ok){
-        joint_state.header.stamp = ros::Time::now();
-        //one message per update, This is 2 since thre are 2 joints which are updated 
-        //at the same time
-        joint_state.name = {"joint1","joint2"};
+    float joint1_pos = ((rand() % 315) / 100.0);
+    float joint2_pos = ((rand() % 315) / 100.0);
 
-        int joint1_pos = (rand()%4);
-        int joint2_pos = (rand()%4);
+    // There is one message per update. This is 2 since 2 joints are updated at a time.
+    // The position is the angle of rotation.
+    joint_state.name.resize(2);
+    joint_state.position.resize(2);
 
-        //position-angle of rotation
-        joint_state.position = {0,0}; //{joint1_pos,joint2_pos};
+    joint_state.name = { "joint1", "joint2" };
+    joint_state.position = { joint1_pos, joint2_pos };
 
-        //actually publishing data
-        std_msgs::String msg;
-        ROS_INFO("%s", msg.data.c_str());
- 
-        //send joint state
-        joint_pub.publish(joint_state);
+    // Outputting data to stdout.
+    std_msgs::String msg;
+    ROS_INFO("%F", joint_state.position[0]);
 
-        loop_rate.sleep();
-     
-    }
+    // Sends joint state to subscribed nodes.
+    joint_pub.publish(joint_state);
 
-    return 0;
+    loop_rate.sleep();
+  }
+
+  return 0;
 }
-
-
-
-
-
