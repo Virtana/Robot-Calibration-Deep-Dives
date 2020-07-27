63dab640ef1202f9a2040b575650ad5daff2a889
RebeccaGibbon <gibbonrm@gmail.com>
2020-07-27 13:33:47 -0400

    Minor changes

--- my_2d_robot/src/joint_states_subscriber.cpp
+++ my_2d_robot/src/joint_states_subscriber.cpp
@@ -6,56 +6,46 @@
 class states
 {
 private:
+  ros::Subscriber sub;
   // Joint positions.
   float position_joint1;
   float position_joint2;
-
   // Joint angle offsets.
   float Theta1_offset;
   float Theta2_offset;
-
   // Link lengths.
   float link_1;
   float link_2;
-
   // End effector positions.
   float x;
   float y;
 
 public:
-  // Callback method.
+  // Callback method to get joint angles.
   void joint_statesCallback(const sensor_msgs::JointState::ConstPtr& msg);
-
-  // Method to get offsets from parameter server.
-  void getOff();
-
   // Method to calculate the position of the end effector.
   void eepos();
-
   // Method to write joint state angles and enf effector position to yaml file
 
   // Contructor.
-  states()
+  states(ros::NodeHandle *n)
   {
     // Initialising length of links (specified in urdf).
     link_1 = 5.0;
     link_2 = 5.0;
 
-    getOff();
+    sub = n->subscribe("joint_states", 1000, &states::joint_statesCallback, this);
+
+    // Getting the joint angle offsets which are set in the launch file.
+    n->getParam("Theta1_offset", Theta1_offset);
+    n->getParam("Theta2_offset", Theta2_offset);
+
+    // Calculating the position of the end effector. 
     eepos();
   }
 };
 
 // Class methods.
-// Getting the joint angle offsets which are set in the launch file.
-void states::getOff()
-{
-  ros::NodeHandle nh;
-
-  nh.getParam("Theta1_offset", Theta1_offset);
-  nh.getParam("Theta2_offset", Theta2_offset);
-}
-
 // Getting joint state information from publisher node.
 void states::joint_statesCallback(const sensor_msgs::JointState::ConstPtr& msg)
 {
@@ -83,11 +73,10 @@ void states::eepos()
 // Main function.
 int main(int argc, char** argv)
 {
-  states my_2d_robo_states;
-
   ros::init(argc, argv, "joint_states_subscriber");
   ros::NodeHandle n;
-  ros::Subscriber sub = n.subscribe("joint_states", 1000, &states::joint_statesCallback, &my_2d_robo_states);
+
+  states my_2d_robo_states = states(&n);
 
   ros::spin();
 
