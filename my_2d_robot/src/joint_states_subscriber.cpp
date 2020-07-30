89afcaa6101c661813faf6a68b29bfa99ebbadb3
RebeccaGibbon <gibbonrm@gmail.com>
2020-07-30 11:31:47 -0400

    Following ros cpp style guide

--- my_2d_robot/src/joint_states_subscriber.cpp
+++ my_2d_robot/src/joint_states_subscriber.cpp
@@ -8,85 +8,85 @@
 #include <boost/filesystem.hpp>
 #include "yaml-cpp/yaml.h"
 
-class states
+class SensorMeasurementData
 {
-private:
-  ros::Subscriber sub;
-  // Joint positions.
-  float position_joint1;
-  float position_joint2;
-  // Joint angle offsets.
-  float Theta1_offset;
-  float Theta2_offset;
-  // Link lengths.
-  float link_1;
-  float link_2;
-  // End effector positions.
-  float x;
-  float y;
-
 public:
+  // Contructor.
+  SensorMeasurementData(ros::NodeHandle* n)
+  {
+    // Initialise length of links (specified in urdf).
+    link_1_ = 5.0;
+    link_2_ = 5.0;
+
+    sub_ = n->subscribe("joint_states", 1000, &SensorMeasurementData::joint_statesCallback, this);
+
+    // Get the joint angle offsets which are set in the launch file.
+    n->getParam("Theta1_offset", theta1_offset_);
+    n->getParam("Theta2_offset", theta2_offset_);
+  }
+
+private:
+  ros::Subscriber sub_;
+  // Joint positions.
+  float position_joint1_;
+  float position_joint2_;
+  // Joint angle offsets.
+  float theta1_offset_;
+  float theta2_offset_;
+  // Link lengths.
+  float link_1_;
+  float link_2_;
+  // End effector positions.
+  float x_;
+  float y_;
+
   // Callback method to get joint angles.
   void joint_statesCallback(const sensor_msgs::JointState::ConstPtr& msg);
   // Method to calculate the position of the end effector.
-  void eepos();
+  void eePos(float joint_1, float joint_2);
   // Method to write the joint state angles and end effector position to yaml file.
-  void save_yaml();
-
-  // Contructor.
-  states(ros::NodeHandle* n)
-  {
-    // Initialise length of links (specified in urdf).
-    link_1 = 5.0;
-    link_2 = 5.0;
-
-    sub = n->subscribe("joint_states", 1000, &states::joint_statesCallback, this);
-
-    // Get the joint angle offsets which are set in the launch file.
-    n->getParam("Theta1_offset", Theta1_offset);
-    n->getParam("Theta2_offset", Theta2_offset);
-  }
+  void saveYaml();
 };
 
 // Class methods.
 // Method to get the joint state information from publisher node.
-void states::joint_statesCallback(const sensor_msgs::JointState::ConstPtr& msg)
+void SensorMeasurementData::joint_statesCallback(const sensor_msgs::JointState::ConstPtr& msg)
 {
   ROS_INFO("I heard: [%F]", msg->position[0]);
 
-  position_joint1 = msg->position[0];
-  position_joint2 = msg->position[1];
+  position_joint1_ = msg->position[0];
+  position_joint2_ = msg->position[1];
 
-  eepos();
+  eePos(position_joint1_, position_joint2_);
 
-  save_yaml();
+  saveYaml();
 }
 
 // Method to calculate the position of the end effector.
-void states::eepos()
+void SensorMeasurementData::eePos(float joint_1, float joint_2)
 {
   // Includes the offsets for joint angles.
-  float Theta1 = position_joint1 - Theta1_offset;
-  float Theta2 = position_joint2 - Theta2_offset;
+  float Theta1 = joint_1 - theta1_offset_;
+  float Theta2 = joint_2 - theta2_offset_;
 
   // Calculates the end effector position.
-  x = (link_1 * cos(Theta1 * M_PI / 180)) + (link_2 * cos((Theta1 + Theta2) * M_PI / 180));
-  y = (link_1 * sin(Theta1 * M_PI / 180)) + (link_2 * sin((Theta1 + Theta2) * M_PI / 180));
+  x_ = (link_1_ * cos(Theta1 * M_PI / 180)) + (link_2_ * cos((Theta1 + Theta2) * M_PI / 180));
+  y_ = (link_1_ * sin(Theta1 * M_PI / 180)) + (link_2_ * sin((Theta1 + Theta2) * M_PI / 180));
 }
 
 // Method to write the joint state angles and end effector position to yaml file.
-void states::save_yaml()
+void SensorMeasurementData::saveYaml()
 {
   YAML::Emitter d_output;
   d_output << YAML::BeginMap;
   d_output << YAML::Key << "joint1 angle";
-  d_output << YAML::Value << position_joint1;
+  d_output << YAML::Value << position_joint1_;
   d_output << YAML::Key << "joint2 angle";
-  d_output << YAML::Value << position_joint2;
+  d_output << YAML::Value << position_joint2_;
   d_output << YAML::Key << "end effector x position";
-  d_output << YAML::Value << x;
+  d_output << YAML::Value << x_;
   d_output << YAML::Key << "end effector y position";
-  d_output << YAML::Value << y;
+  d_output << YAML::Value << y_;
   d_output << YAML::EndMap;
 
   // Gets the package filepath.
@@ -129,7 +129,7 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "joint_states_subscriber");
   ros::NodeHandle n;
 
-  states my_2d_robo_states = states(&n);
+  SensorMeasurementData my_2d_robo_states = SensorMeasurementData(&n);
 
   ros::spin();
 
