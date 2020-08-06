8271143fb745b5ba5eb0830ad74cd4f1ceb8e355
RebeccaGibbon <gibbonrm@gmail.com>
2020-08-06 11:10:35 -0400

    fixed yaml parsing

--- my_2d_robot/src/joint_states_subscriber.h
+++ my_2d_robot/src/joint_states_subscriber.h
@@ -52,33 +52,8 @@ private:
   // Counts the number of data points received from publisher node.
   int data_count_;
   int data_num_max_;
-<<<<<<< HEAD
 };
 
-
-=======
-
-  // Callback method to get joint angles.
-  void jointStatesCallback(const sensor_msgs::JointState::ConstPtr& msg);
-
-  // Method to write the joint state angles and end effector position to yaml file.
-  std::string saveJointAnglesEepos(Vector2d end_effector_position);
-
-  // Method to calculate the position of the end effector.
-  Vector2d eePos(double joint_1, double joint_2)
-  {
-    // Includes the offsets for joint angles.
-    double Theta1 = joint_1 - (theta1_offset_ * M_PI / 180);
-    double Theta2 = joint_2 - (theta2_offset_ * M_PI / 180);
-
-    // Calculates the end effector position.
-    Vector2d position;
-    position << (link_1_ * cos(Theta1 * M_PI / 180)) + (link_2_ * cos((Theta1 + Theta2) * M_PI / 180)),
-        (link_1_ * sin(Theta1 * M_PI / 180)) + (link_2_ * sin((Theta1 + Theta2) * M_PI / 180));
-
-    return position;
-  }
-};
 
 // Method to get the joint state information from publisher node.
 void SensorMeasurementData::jointStatesCallback(const sensor_msgs::JointState::ConstPtr& msg)
@@ -104,3 +79,29 @@ void SensorMeasurementData::jointStatesCallback(const sensor_msgs::JointState::C
   data_count_ += 1;
   stream << output_data_yaml_ << std::endl;
   fout << stream.rdbuf();
+
+  if (data_count_ == data_num_max_)
+  {
+    ros::shutdown();
+  }
+}
+
+// Method to write the joint state angles and end effector position to yaml file.
+std::string SensorMeasurementData::saveJointAnglesEepos(Vector2d end_effector_position)
+{
+  std::string output_data_;
+  YAML::Emitter d_output;
+  d_output << YAML::BeginSeq;
+  d_output << YAML::BeginMap << YAML::Key << "joint angles" << YAML::Value << YAML::Flow << YAML::BeginSeq
+           << position_joint1_ << position_joint2_ << YAML::EndSeq;
+  d_output << YAML::Key << "end effector position" << YAML::Value << YAML::Flow << YAML::BeginSeq
+           << end_effector_position(0) << end_effector_position(1) << YAML::EndSeq << YAML::EndMap;
+  d_output << YAML::EndSeq;
+
+  output_data_.append(d_output.c_str());
+  output_data_.append("\n");
+
+  return output_data_;
+}
+
+#endif
