984f0f38d2db96fe77112a57dc6be227cb85d6c8
RebeccaGibbon <gibbonrm@gmail.com>
2020-08-21 21:01:02 -0400

    using kdl to calculate end effector position

--- robot_3d/src/joint_states_subscriber_3d.cpp
+++ robot_3d/src/joint_states_subscriber_3d.cpp
@@ -1,6 +1,8 @@
 #include <ros/ros.h>
 #include <sensor_msgs/JointState.h>
 #include "std_msgs/String.h"
+#include <tf/transform_broadcaster.h>
+
 #include <kdl_parser/kdl_parser.hpp>
 
 #include <kdl/chain.hpp>
@@ -46,26 +48,30 @@ void JointMeasurementData::kdlTest(double joint_positions[])
   // kdl parser --think this works since I get no errors :)
   KDL::Tree fanuc_tree;
   std::string fanuc_robot;
+
   ros::NodeHandle n;
   n.param("/robot_description", fanuc_robot, std::string());
   if (!kdl_parser::treeFromString(fanuc_robot, fanuc_tree))
   {
     ROS_ERROR("Failed to construct kdl tree");
-    //return false;
+    // return false;
   }
 
   // orocos_kdl to calculate end effector
-  // CReate kdl chain.
+  // Create kdl chain.
   KDL::Chain chain;
+  fanuc_tree.getChain("base_link", "link_6", chain);
+
   KDL::ChainFkSolverPos_recursive fksolver = KDL::ChainFkSolverPos_recursive(chain);
   // Create joint array.
   unsigned int nj = chain.getNrOfJoints();
   KDL::JntArray jointpositions = KDL::JntArray(nj);
+  ROS_INFO("NrOfJoints =%d", nj);
 
   // assign joint positions...
   for (unsigned int i = 0; i < nj; i++)
   {
-    jointpositions(i) = position[i];
+    jointpositions(i) = joint_positions[i];
   }
 
   // Create the frame that will contain the results
@@ -77,13 +83,14 @@ void JointMeasurementData::kdlTest(double joint_positions[])
   if (kinematics_status >= 0)
   {
     std::cout << cartpos << std::endl;
-    ROS_INFO("%s", "something's happening");
-    printf("%s \n", "Succes, thanks KDL!");
+    ROS_INFO("%s \n", "Succes, thanks KDL!");
   }
   else
   {
-    printf("%s \n", "Error: could not calculate forward kinematics :(");
+    ROS_INFO("%s \n", "Error: could not calculate forward kinematics :(");
   }
+
+  
 }
 
 int main(int argc, char** argv)
@@ -91,49 +98,8 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "joint_states_subscriber_3d");
 
   ros::NodeHandle n;
-  // ros::Subscriber sub = n.subscribe("joint_states", 1000, jointStatesCallback);
-
+  
   JointMeasurementData robot_3d_states = JointMeasurementData(&n);
-
-  //   // kdl parser --think this works since I get no errors :)
-  //   KDL::Tree fanuc_tree;
-  //   std::string fanuc_robot;
-  //   n.param("/robot_description", fanuc_robot, std::string());
-  //   if (!kdl_parser::treeFromString(fanuc_robot, fanuc_tree))
-  //   {
-  //     ROS_ERROR("Failed to construct kdl tree");
-  //     return false;
-  //   }
-
-  //   // orocos_kdl to calculate end effector
-  //   // CReate kdl chain.
-  //   KDL::Chain chain;
-  //   KDL::ChainFkSolverPos_recursive fksolver = KDL::ChainFkSolverPos_recursive(chain);
-  //   // Create joint array.
-  //   unsigned int nj = chain.getNrOfJoints();
-  //   KDL::JntArray jointpositions = KDL::JntArray(nj);
-
-  //   // assign joint positions...
-  //   for (unsigned int i = 0; i < nj; i++)
-  //   {
-  //     jointpositions(i) = robot_3d_states.position[i];
-  //   }
-
-  //   // Create the frame that will contain the results
-  //   KDL::Frame cartpos;
-
-  //   // Calculate forward position kinematics
-  //   bool kinematics_status;
-  //   kinematics_status = fksolver.JntToCart(jointpositions, cartpos);
-  //   if (kinematics_status >= 0)
-  //   {
-  //     std::cout << cartpos << std::endl;
-  //     printf("%s \n", "Succes, thanks KDL!");
-  //   }
-  //   else
-  //   {
-  //     printf("%s \n", "Error: could not calculate forward kinematics :(");
-  //   }
 
   ros::spin();
 
