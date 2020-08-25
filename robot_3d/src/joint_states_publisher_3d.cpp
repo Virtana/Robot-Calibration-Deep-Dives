dc77e48faed3d41b507912e8a89a5e9c2a30d971
RebeccaGibbon <gibbonrm@gmail.com>
2020-08-24 20:40:28 -0400

    added new line to launch file and fixed angle generation in publisher

--- robot_3d/src/joint_states_publisher_3d.cpp
+++ robot_3d/src/joint_states_publisher_3d.cpp
@@ -5,6 +5,13 @@
 #include <sensor_msgs/JointState.h>
 #include "std_msgs/String.h"
 
+double genRandomAngle(double lower_limit, double upper_limit)
+{
+  double angle =
+      lower_limit + static_cast<double>(rand()) / (static_cast<double>(RAND_MAX / (upper_limit - lower_limit)));
+  return angle;
+}
+
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "joint_states_publisher_3d");
@@ -26,20 +33,22 @@ int main(int argc, char** argv)
 
     // Randomly generating joint angles within ranges specified in .xacro file.
     // Generating joint angles for joints 1 and 3 within the range [-3.1415, 3.1415].
-    double joint_1_3 = (rand() % 62831 + (-31415)) / 10000.0;
+    double joint_1 = genRandomAngle(-3.1415, 3.1415);
+    double joint_3 = genRandomAngle(-3.1415, 3.1415);
     // Generating joint angles for joints 4 and 6 within the range [-6.2831, 6.2831].
-    double joint_4_6 = (rand() % 125663 + (-62831)) / 10000.0;
+    double joint_4 = genRandomAngle(-6.2831, 6.2831);
+    double joint_6 = genRandomAngle(-6.2831, 6.2831);
     // Generating joint angles for joint 2 within the range [-1.5707, 2.7052].
-    double joint_2 = (rand() % 42760 + (-15707)) / 10000.0;
+    double joint_2 = genRandomAngle(-1.5707, 2.7052);
     // Generating joint angles for joint 5 within the range [-2.1816, 2.1816].
-    double joint_5 = (rand() % 43633 + (-21816)) / 10000.0;
+    double joint_5 = genRandomAngle(-2.1816, 2.1816);
 
     // There is one message per update. This is 2 since 2 joints are updated at a time.
     joint_state.name.resize(6);
     joint_state.position.resize(6);
 
     joint_state.name = { "joint_1", "joint_2", "joint_3", "joint_4", "joint_5", "joint_6" };
-    joint_state.position = { joint_1_3, joint_2, joint_1_3, joint_4_6, joint_5, joint_4_6 };
+    joint_state.position = { joint_1, joint_2, joint_3, joint_4, joint_5, joint_6 };
 
     // Outputs data to stdout.
     std_msgs::String msg;
